7161289ea80e41577a1d2abaea42b167ef280558
hansaskov <hansaskov1999@gmail.com>
2023-12-30 01:21:11 +0100

    Update website

--- src/consts.ts
+++ src/consts.ts
@@ -1,5 +1,5 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'Askov\' Blog';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Hans Askov';
+export const SITE_DESCRIPTION = 'Personal blog for Hans Askov with an about me page';

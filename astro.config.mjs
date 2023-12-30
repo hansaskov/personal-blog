2910175f4d683f442edadf4bdd7c21381bd9e8f2
hansaskov <hansaskov1999@gmail.com>
2023-12-30 01:32:18 +0100

    Add vercel adapter

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,16 @@
 import { defineConfig } from 'astro/config';
 import mdx from '@astrojs/mdx';
-
 import sitemap from '@astrojs/sitemap';
+
+import vercel from "@astrojs/vercel/serverless";
 
 // https://astro.build/config
 export default defineConfig({
-	site: 'https://example.com',
-	integrations: [mdx(), sitemap()],
-	image: {
-		domains: ["/images"],
-	  }
+  site: 'https://example.com',
+  integrations: [mdx(), sitemap()],
+  image: {
+    domains: ["/images"]
+  },
+  output: "server",
+  adapter: vercel()
 });
